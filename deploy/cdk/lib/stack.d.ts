f39efda5858f2fcec1a7fc5b5abec87e674bdea2
R. DeVitto <53380910+rdevitto86@users.noreply.github.com>
2026-06-26 16:06:56 -0500

    Remove more files in gitignore

--- deploy/cdk/lib/stack.d.ts
+++
@@ -1,4 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { EnvConfig } from './config';
-export declare function createStack(scope: Construct, id: string, cfg: EnvConfig, props: cdk.StackProps): cdk.Stack;
